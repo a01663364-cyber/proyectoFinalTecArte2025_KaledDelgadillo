8d0d8b9a5cc0d798cd3b748beef715ed66083944
Kaled <A01663364@tec.mx>
2025-11-02 17:05:48 -0600

    Codigo comentado

--- figurasPrincipal.py
+++ figurasPrincipal.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
+# Leer el archivo separado por tabulaciones
 dataFile = pd.read_csv("figuras.csv", sep="\t")
 
 dataFile.columns = dataFile.columns.str.strip().str.upper()
 
+# Limpiar nombres de columnas
 dataFile ["MEDIDA1"] = pd.to_numeric(dataFile["MEDIDA1"], errors="coerce")
 if "MEDIDA2" in dataFile.columns:
         dataFile ["MEDIDA2"] = pd.to_numeric(dataFile["MEDIDA2"], errors="coerce")
 
+# Funciones para las áreas
 def triangulo (base, altura):
         return (base * altura) / 2
 def rectangulo (base, altura):
@@ -15,11 +18,14 @@ def rectangulo (base, altura):
 def circulo(radio):
         return 3.1416 * (radio ** 2)
 
+# Lista de resultados
 areas = []
 
+# Recorrer filas
 for index, row in dataFile.iterrows ():
         figura = str(row["FIGURA"] ).strip().lower()
 
+# Traducir letras a nombres completos
         if figura == "t":
                 figura = "triangulo"
         elif figura == "r":
@@ -45,6 +51,7 @@ for index, row in dataFile.iterrows ():
         else:
                 print(f"Fila {index}: Datos incompletos o figura no reconocida → FIGURA={row['FIGURA']}")
 
+# Agregar columna con áreas
 dataFile["AREA"] = areas
 print("\n--- Tabla final con áreas ---")
 print (dataFile)
